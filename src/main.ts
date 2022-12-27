fa1113220c2c0f3c8fba4907bb9933ea466f5fd1
Dcolor Wei <corfer@yeah.net>
2022-12-27 16:58:09 +0800

    config: 路由跳转

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { i18n } from './local'
 import "./style.css"
 import "./assets/font/font.css"
+import axios from 'axios'
 
-createApp(App).use(router).use(i18n).mount('#app')
+axios.defaults.baseURL = "https://www.noworry.cloud/"
+// axios.defaults.baseURL = "http://127.0.0.1:3000"
+createApp(App).use(router).mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,47 @@
+import { getCookie } from '@/method/cookie';
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    component: ()=>import("@/views/HomeView.vue")
+    redirect:"/home"
   },
   {
-    path: '/contact',
-    component: ()=>import("@/views/ContactView.vue")
+    path: '/login',
+    component: () => import("@/views/LoginView.vue")
+  },
+  {
+    path: '/home',
+    component: () => import("@/views/HomeView.vue")
+  },
+  {
+    path: '/commodity',
+    component: () => import("@/views/CommodityView.vue")
+  },
+  {
+    path: '/order',
+    component: () => import("@/views/OrderView.vue")
+  },
+  {
+    name: "detail",
+    path: '/detail',
+    component: () => import("@/views/OrderDetailView.vue"),
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from) => {
+  const time = Date.now();
+  if (time - Number(getCookie("token")) < 1000 * 3600 * 4 || to.fullPath == "/login") {
+    document.cookie = `token=${time}`;
+    return true
+  } else {
+    return { path: "/login" }
+  }
 })
 
 export default router
