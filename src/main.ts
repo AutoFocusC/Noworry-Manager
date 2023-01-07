d9502ac7b54d79a8ed818e9dd8e0f651f245d3ac
Dcolor Wei <corfer@yeah.net>
2023-01-07 13:50:09 +0800

    feature: axios请求强制携带token

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,14 @@ import router from "./router";
 import "./style.css";
 import "./assets/font/font.css";
 import axios from "axios";
+import { getCookie } from "./method/cookie";
 
+//axios设置
 axios.defaults.baseURL = "https://4997xs4632.goho.co/";
-
-// axios.defaults.baseURL = "http://127.0.0.1:3000"
+axios.interceptors.request.use(config => {
+    console.log(config)
+    config.headers!['token'] = getCookie('token')
+    return config;
+})
+// axios.defaults.baseURL = "http://127.0.0.1:39443"
 createApp(App).use(router).mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,6 @@
 module.exports = {
   publicPath: '/',
   assetsDir: "./static",
-  indexPath: "./templates/index.html",
-  // devServer: {
-  //   proxy: {
-  //     "/v1": {
-  //       target: 'https://mobile.51fubaba.cn:8443/v1', // 对应的代理地址
-  //       changeOrigin: true,
-  //       pathRewrite: {
-  //         '^/api': ''
-  //     }
-  //     },
-  //   },
-  // }
+  indexPath: "index.html",
+
 }
